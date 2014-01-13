9c06bbea6a1efef8782055a4c33d3d66bb612d6b
Dominic Frost <frostationdevice@gmail.com>
2014-01-12 17:06:55 -0700

    bug search wins

--- safebot/RobotPlayer.java
+++ safebot/RobotPlayer.java
@@ -12,6 +12,7 @@ public class RobotPlayer {
     static RobotController rc;
     static double cowCoords[][];
     static int map[][];
+    static int cornersMap[][];
     static MapLocation maxCows = null;
     static Boolean firstRun = true;
     static ArrayList<MapLocation> corners= new ArrayList<MapLocation>();
@@ -24,11 +25,13 @@ public class RobotPlayer {
         Boolean hasOrders = false;
         Boolean start = false;
         MapLocation corner = null;
+        map = new int[rc.getMapWidth()][rc.getMapHeight()];
+        cornersMap = new int [rc.getMapWidth() + 2][rc.getMapHeight() + 2];
         while(true) {
 			if (rc.getType() == RobotType.HQ) {
 				try {					
 					//Check if a robot is spawnable and spawn one if it is
-					if (rc.isActive() && rc.senseRobotCount() < 25) {
+					if (rc.isActive() && rc.senseRobotCount() < 1) {
 						Direction toEnemy = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
 						if (rc.senseObjectAtLocation(rc.getLocation().add(toEnemy)) == null) {
 							rc.spawn(toEnemy);
@@ -38,12 +41,12 @@ public class RobotPlayer {
                     if(firstRun) {
                         rc.broadcast(0, 0);
                         firstRun = false;
-                        drawMap();
+                        RobotUtil.assessMap(rc, map, cornersMap);
                         locateCorners();
 //                        System.out.println("write: " + corners.size());
                         rc.broadcast(0, corners.size());
                         for(int i = 0; i < corners.size(); i++) {
-                            rc.broadcast(i + 1, mapLocToInt(corners.get(i)));
+                            rc.broadcast(i + 1, RobotUtil.mapLocToInt(corners.get(i)));
                         }
                     }
 //                    rc.setIndicatorString(1, "" + rc.senseTerrainTile(new MapLocation(10, 10)).ordinal());
@@ -59,7 +62,7 @@ public class RobotPlayer {
 //                        System.out.println( "read: " + rc.readBroadcast(0));
                         if(rc.readBroadcast(0) != 0) {
                             for(int i = 0; i < rc.readBroadcast(0); i++) {
-                                corners.add(intToMapLoc(rc.readBroadcast(i + 1)));
+                                corners.add(RobotUtil.intToMapLoc(rc.readBroadcast(i + 1)));
                             }
                             start = true;
                         }
@@ -71,6 +74,9 @@ public class RobotPlayer {
                                 hasOrders = true;
                             }
                             if(hasOrders) {
+                                map = RobotUtil.assessMap(rc, map, cornersMap);
+                                RobotUtil.logMap(map);
+                                RobotUtil.bugPath(rc.getLocation(), corner, map);
                                 Direction dir = rc.getLocation().directionTo(corner);
                                 if(rc.getLocation().x != corner.x || rc.getLocation().y != corner.y) {
                                     if (rc.canMove(dir)) {
@@ -129,26 +135,6 @@ public class RobotPlayer {
         return new MapLocation(xLoc, yLoc);
     }
 
-    public static void drawMap() {
-        mapWidth = rc.getMapWidth();
-        int ogMapWidth = mapWidth;
-        mapHeight = rc.getMapHeight();
-        map = new int[mapWidth + 2][mapHeight + 2];
-        for (int i = 0; i < mapHeight + 2; i++) {
-            for (int j = 0; j < mapWidth + 2; j++) {
-                int tile;
-                if(i == 0 || j == 0 || i == ogMapWidth + 1 || j == mapHeight + 1) { // pad the map to easily find corners
-                    tile = 2;
-                } else {
-                    tile = rc.senseTerrainTile(new MapLocation(j - 1, i - 1)).ordinal();
-                }
-                map[i][j] = tile > 2 ? 2 : tile;
-                map[ogMapWidth + 1 - i][mapHeight + 1 - j] = tile > 2 ? 2 : tile;
-            }
-            mapWidth--;
-        }
-    }
-
     public static void locateCorners() {
         mapWidth = rc.getMapWidth();
         mapHeight = rc.getMapHeight();
@@ -161,45 +147,27 @@ public class RobotPlayer {
                 // x - P - -
                 // x - - - -
                 // x - - - -
-                if (map[i][j] != 2) {
-                    if (map[i-1][j-1] == 2 && map[i][j-1] == 2 && map[i-1][j] == 2) {           // top left corner
+                if (cornersMap[i][j] != 2) {
+                    if (cornersMap[i-1][j-1] == 2 && cornersMap[i][j-1] == 2 && cornersMap[i-1][j] == 2) {           // top left corner
                         corners.add(new MapLocation((j - 1) + 1, (i - 1) + 1));
                         map[i+2][j+2] = 3;
 //                        System.out.println("adding corner from 1: " + "(" + (j - 1 + 2) + ", " + (i - 1 + 2) + ")");
-                    } else if (map[i+1][j-1] == 2 && map[i][j-1] == 2 && map[i+1][j] == 2) {    // top right corner
+                    } else if (cornersMap[i+1][j-1] == 2 && cornersMap[i][j-1] == 2 && cornersMap[i+1][j] == 2) {    // top right corner
                         corners.add(new MapLocation((j - 1) + 1, (i - 1) - 1));
-                        map[i-2][j+2] = 3;
+                        cornersMap[i-2][j+2] = 3;
 //                        System.out.println("adding corner from 2: " + "(" + (j - 1 + 2) + ", " + (i - 1 - 2) + ")");
-                    } else if (map[i+1][j+1] == 2 && map[i][j+1] == 2 && map[i+1][j] == 2) {    // bottom right corner
+                    } else if (cornersMap[i+1][j+1] == 2 && cornersMap[i][j+1] == 2 && cornersMap[i+1][j] == 2) {    // bottom right corner
                         corners.add(new MapLocation((j - 1) - 1, (i - 1) - 1));
-                        map[i-2][j-2] = 3;
+                        cornersMap[i-2][j-2] = 3;
 //                        System.out.println("adding corner from 3: " + "(" + (j - 1 - 2) + ", " + (i - 1 - 2) + ")");
-                    } else if (map[i-1][j+1] == 2 && map[i][j+1] == 2 && map[i-1][j] == 2) {    // bottom left corner
+                    } else if (cornersMap[i-1][j+1] == 2 && cornersMap[i][j+1] == 2 && cornersMap[i-1][j] == 2) {    // bottom left corner
                         corners.add(new MapLocation((j - 1) - 1, (i - 1) + 1));
-                        map[i+2][j-2] = 3;
+                        cornersMap[i+2][j-2] = 3;
 //                        System.out.println("adding corner from 4: " + "(" + (j - 1 - 2) + ", " + (i - 1 + 2) + ")");
                     }
                 }
             }
         }
 //        logMap();
-    }
-
-    public static void logMap() {
-        System.out.print("\n");
-        for (int i = 1; i < mapWidth + 1; i++) {
-            for (int j = 1; j < mapHeight + 1; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
-    }
-
-    public static int mapLocToInt(MapLocation m){
-        return (m.x*100 + m.y);
-    }
-
-    public static MapLocation intToMapLoc(int i){
-        return new MapLocation(i/100,i%100);
     }
 }
